e71268c1488483bb4eb074094e3ed99e6a9d7fac
MadFury94 <onochieazukaeme@gmail.com>
2023-08-03 00:52:38 +0100

    heroslide 2

---
+++ layouts/HoverButtons.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="rounded-lg bg-red-500 h-44 w-full">
+      Btn1
+      <i class="fa-duotone fa-jug-detergent"></i>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <HeroSlide />
+    <HoverButtons />
   </div>
 </template>
 
 <script setup lang="ts">
 import HeroSection from "../layouts/HeroSection.vue";
 import HeroSlide from "../layouts/HeroSlide.vue";
+import HoverButtons from "../layouts/HoverButtons.vue";
 </script>
