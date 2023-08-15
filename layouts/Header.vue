c3f89cf495ddfa2f35e2ebac27255e3fd02812b5
MadFury94 <onochieazukaeme@gmail.com>
2023-08-15 10:17:56 +0100

    refactor headermenu

--- layouts/Header.vue
+++ layouts/Header.vue
@@ -11,7 +11,7 @@
         </NuxtLink>
       </div>
     </div>
-    <header  class="bg-white">
+    <header class="bg-white">
       <nav
         class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
@@ -46,47 +46,50 @@
           >
         </div>
       </nav>
-      <Dialog
-        as="div"
-        class="lg:hidden"
-        @close="mobileMenuOpen = false"
-        :open="mobileMenuOpen"
-      >        <div class="fixed inset-0 z-10" />
-        <DialogPanel
-          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      <ClientOnly>
+        <Dialog
+          as="div"
+          class="lg:hidden"
+          @close="mobileMenuOpen = false"
+          :open="mobileMenuOpen"
         >
-          <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
-              <img
-                class="h-8 w-auto"
-                src="https://www.sprynxmulti.com/wp-content/uploads/2020/03/sprynx_new_logo-color-01.png"
-                alt=""
-              />
-            </a>
-            <button
-              type="button"
-              class="-m-2.5 rounded-md p-2.5 text-gray-700"
-              @click="mobileMenuOpen = false"
-            >
-              <span class="fas fa-window-close">Close menu</span>
-            </button>
-          </div>
-          <div class="mt-6 flow-root">
-            <div class="-my-6 divide-y divide-gray-500/10">
-              <div class="space-y-2 py-6">
-                <NuxtLink
-                  v-for="(item, index) in navigation"
-                  :key="index"
-                  :to="{ name: item.route.name }"
-                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  <i :class="item.icon"></i> {{ item.name }}</NuxtLink
-                >
+          <div class="fixed inset-0 z-10" />
+          <DialogPanel
+            class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+          >
+            <div class="flex items-center justify-between">
+              <a href="#" class="-m-1.5 p-1.5">
+                <img
+                  class="h-8 w-auto"
+                  src="https://www.sprynxmulti.com/wp-content/uploads/2020/03/sprynx_new_logo-color-01.png"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                class="-m-2.5 rounded-md p-2.5 text-gray-700"
+                @click="mobileMenuOpen = false"
+              >
+                <span class="fas fa-window-close">Close menu</span>
+              </button>
+            </div>
+            <div class="mt-6 flow-root">
+              <div class="-my-6 divide-y divide-gray-500/10">
+                <div class="space-y-2 py-6">
+                  <NuxtLink
+                    v-for="(item, index) in navigation"
+                    :key="index"
+                    :to="{ name: item.route.name }"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    <i :class="item.icon"></i> {{ item.name }}</NuxtLink
+                  >
+                </div>
               </div>
             </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
+          </DialogPanel>
+        </Dialog>
+      </ClientOnly>
     </header>
   </div>
 </template>
@@ -94,8 +97,6 @@
 <script setup>
 import { Dialog, DialogPanel } from "@headlessui/vue";
 import { ref } from "vue";
-
-
 
 const navigation = [
   {
