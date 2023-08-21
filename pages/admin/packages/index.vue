23d85d36ee1a01dec0408c22a64f21f9c2ad9873
katalyst33 <>
2023-08-21 15:16:34 +0100

    created all packages component

--- pages/admin/packages/index.vue
+++ pages/admin/packages/index.vue
@@ -5,28 +5,35 @@
     <button class="bg-green-500 p-2">Create Package</button>
 
     <div class="form">
-      <div class="w-80 mx-auto border p-10">
+      <div class="w-80 mx-auto border p-10 grid grid-cols-1 gap-y-6">
         <div class="relative">
-          <label for="name">Price</label>
-          <input type="number" name="name" id="name" placeholder="Jane Smith" />
+          <label id="name">Name</label>
+          <input v-model="form.name" type="text" placeholder="Silver" />
         </div>
+        <div class="relative">
+          <label id="duration">Duration</label>
+          <input v-model="form.duration" type="text" placeholder="Silver" />
+        </div>
+        <div class="relative">
+          <label id="price">Price</label>
+          <input v-model="form.price" type="number" placeholder="N20,000" />
+        </div>
+
+
+        <button>Add Package</button>
       </div>
     </div>
 
     <!-- you will need to handle a loading state -->
-    <div v-if="pending">Loading ...</div>
-    <div v-else>
-      <div v-for="item in data" :key="item.uuid">
-        <!-- do something -->
-
-        {{ item }}
-      </div>
-    </div>
+   <AllPackages/>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useAxiosFetch } from "../../../http";
+
+import AllPackages from "../packages/components/allPackages.vue";
+
 
 definePageMeta({
   layout: "admin-layout",
@@ -35,9 +42,23 @@ definePageMeta({
   description: "This is the all packages page of our company",
 });
 
-const [pending, getData, data, error] = useAxiosFetch("packages/all");
 
-onMounted(getData);
+const [pending, getData, data, error] = useAxiosFetch("packages/all",{
+    
+});
+
+const form = {
+  duration: "8 to 9 hours",
+  name: "PREMIUM DETAILING",
+  price: 35000,
+};
+
+
+
+const addPackage = () => {
+  console.log(form);
+  // onMounted(getData);
+};
 </script>
 
 <style scoped></style>
