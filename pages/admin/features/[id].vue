56ea2cec6b178f478ba015fd925e7bfa4003ee65
katalyst33 <>
2023-08-21 13:19:06 +0100

    get one feature

---
+++ pages/admin/features/[id].vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-green-500 p-40">
+    {{ id }}
+    <!-- you will need to handle a loading state -->
+    <div v-if="pending">Loading ...</div>
+    <div v-else>
+      <div v-for="item in data">
+        {{ item }}
+
+        <!-- do something -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAxiosFetch } from "../../../http";
+
+definePageMeta({
+  name: "one-feature",
+  layout: "admin-layout",
+});
+
+const $route = useRoute();
+
+const id = $route.params.id;
+
+const [pending, getData, data, error] = useAxiosFetch(`packages/one-feature/`, {
+  id: id,
+});
+
+onMounted(getData);
+</script>
+
+<style scoped></style>
