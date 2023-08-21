cf9576cd63bce3e285f87c41ad44e185824ca9ac
katalyst33 <>
2023-08-21 16:53:19 +0100

    added modal

--- pages/admin/packages/components/AllPackages.vue
+++ pages/admin/packages/components/AllPackages.vue
@@ -3,23 +3,7 @@
   <div v-if="pending">Loading ...</div>
   <div v-else>
     <div class="px-4 sm:px-6 lg:px-8">
-      <div class="sm:flex sm:items-center">
-        <div class="sm:flex-auto">
-          <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
-          <p class="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
-        </div>
-        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Add user
-          </button>
-        </div>
-      </div>
+    
       <div class="mt-8 flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div
@@ -110,6 +94,9 @@ import { useAxiosRequest } from "../../../../http";
 //
 const [pending, getData, data, error] = useAxiosRequest("packages/all");
 onMounted(getData);
+
+
+
 </script>
 
 <style scoped></style>
--- pages/admin/packages/index.vue
+++ pages/admin/packages/index.vue
@@ -4,27 +4,52 @@
 
     <button class="bg-green-500 p-2">Create Package</button>
 
-    <div class="form">
-      <div class="w-80 mx-auto border p-10 grid grid-cols-1 gap-y-6">
-        <div class="relative">
-          <label id="name">Name</label>
-          <input v-model="form.name" type="text" placeholder="Silver" />
-        </div>
-        <div class="relative">
-          <label id="duration">Duration</label>
-          <input v-model="form.duration" type="text" placeholder="Silver" />
-        </div>
-        <div class="relative">
-          <label id="price">Price</label>
-          <input v-model="form.price" type="number" placeholder="N20,000" />
-        </div>
+    <Modal max-size="max-w-md" @on-close="modalActive = !modalActive" class="" v-if="modalActive">
+      <div class="form">
+        <div
+          class="bg-gray-50 mx-auto border pt-14 px-4 pb-10 grid grid-cols-1 gap-y-6"
+        >
+          <div class="relative">
+            <label id="name">Name</label>
+            <input v-model="form.name" type="text" placeholder="Silver" />
+          </div>
+          <div class="relative">
+            <label id="duration">Duration</label>
+            <input v-model="form.duration" type="text" placeholder="Silver" />
+          </div>
+          <div class="relative">
+            <label id="price">Price</label>
+            <input v-model="form.price" type="number" placeholder="N20,000" />
+          </div>
 
-        <button @click="createPackage">Add Package</button>
+          <button class="btn" @click="createPackage">Add Package</button>
+        </div>
       </div>
+    </Modal>
+
+    <div class="p-20">
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+          <p class="mt-2 text-sm text-gray-700">
+            A list of all the users in your account including their name, title,
+            email and role.
+          </p>
+        </div>
+        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            @click="modalActive = !modalActive"
+            type="button"
+            class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add Package
+          </button>
+        </div>
+      </div>
+      <AllPackages />
     </div>
 
     <!-- you will need to handle a loading state -->
-    <AllPackages />
   </div>
 </template>
 
@@ -39,6 +64,8 @@ definePageMeta({
   title: "All Packages",
   description: "This is the all packages page of our company",
 });
+
+const modalActive = ref(false);
 
 const form = ref({
   duration: "8 to 9 hours",
