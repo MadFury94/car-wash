8f8b2ebfb08491c2183c86645706825b5e4eeba6
katalyst33 <>
2023-08-21 10:11:30 +0100

    update data

--- pages/packages.vue
+++ pages/packages.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <PackagesSection :package-data="tiers" />
+    <PackagesSection :package-data="packageData" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { package_detail_data } from "../store/data";
 import PackagesSection from "./sections/PackagesSection.vue";
 
 definePageMeta({
@@ -14,153 +15,9 @@ definePageMeta({
   description: "This is the package page of our company",
 });
 
-const tiers = [
-  {
-    title: "Small Size Car",
-    value: "small-size-car",
-    packages: [
-      {
-        name: "PREMIUM DETAILING",
-        id: "tier-freelancer",
-        href: "#",
-        price: "85,000",
-        description: "The essentials to provide your best work for clients.",
-        features: ["INTERIOR", "EXTERIOR", "PAINT", "ENGINE"],
-        mostPopular: false,
-      },
-      {
-        name: "Gold Package",
-        id: "tier-hobby",
-        href: "#",
-        price: "85,000",
-        description: "The essentials to provide your best work for clients.",
-        features: ["INTERIOR", "EXTERIOR", "PAINT"],
 
-        mostPopular: false,
-      },
 
-      {
-        name: "DIAMOND PACKAGE",
-        id: "tier-startup",
-        href: "#",
-        price: "60",
-        description: "A plan that scales with your rapidly growing business.",
-        features: ["INTERIOR", "EXTERIOR", "ENGINE"],
-
-        mostPopular: true,
-      },
-      {
-        name: "SILVER PACKAGE",
-        id: "tier-startup",
-        href: "#",
-        price: "₦60",
-        description: "A plan that scales with your rapidly growing business.",
-        features: ["INTERIOR", "EXTERIOR", "PAINT"],
-
-        mostPopular: false,
-      },
-      {
-        name: "BRONZE PACKAGE",
-        id: "tier-startup",
-        href: "#",
-        price: "60",
-        description: "A plan that scales with your rapidly growing business.",
-        features: ["INTERIOR", "EXTERIOR"],
-
-        mostPopular: false,
-      },
-    ],
-  },
-  {
-    title: "Mid Size Car",
-    value: "mid-size-car",
-    packages: [
-      {
-        name: "Mid -Express Wash",
-        id: "tier-hobby",
-        href: "#",
-        price: "15",
-        description: "The essentials to provide your best work for clients.",
-        features: ["5 products", "Up to 1,000 subscribers", "Basic analytics"],
-        mostPopular: false,
-      },
-      {
-        name: "Mid  Supreme Wash",
-        id: "tier-freelancer",
-        href: "#",
-        price: "30",
-        description: "The essentials to provide your best work for clients.",
-        features: [
-          "5 products",
-          "Up to 1,000 subscribers",
-          "Basic analytics",
-          "48-hour support response time",
-        ],
-        mostPopular: false,
-      },
-      {
-        name: "mid Utra Fullservice",
-        id: "tier-startup",
-        href: "#",
-        price: "60",
-        description: "A plan that scales with your rapidly growing business.",
-        features: [
-          "25 products",
-          "Up to 10,000 subscribers",
-          "Advanced analytics",
-          "24-hour support response time",
-          "Marketing automations",
-        ],
-        mostPopular: true,
-      },
-    ],
-  },
-
-  {
-    title: "Full Size Car",
-    value: "full-size-car",
-    packages: [
-      {
-        name: "full-- Hobby999",
-        id: "tier-hobby",
-        href: "#",
-        price: "15",
-        description: "The essentials to provide your best work for clients.",
-        features: ["5 products", "Up to 1,000 subscribers", "Basic analytics"],
-        mostPopular: false,
-      },
-      {
-        name: "Full --- Freelancer",
-        id: "tier-freelancer",
-        href: "#",
-        price: "30",
-        description: "The essentials to provide your best work for clients.",
-        features: [
-          "5 products",
-          "Up to 1,000 subscribers",
-          "Basic analytics",
-          "48-hour support response time",
-        ],
-        mostPopular: false,
-      },
-      {
-        name: "full ---Startup",
-        id: "tier-startup",
-        href: "#",
-        price: "60",
-        description: "A plan that scales with your rapidly growing business.",
-        features: [
-          "25 products",
-          "Up to 10,000 subscribers",
-          "Advanced analytics",
-          "24-hour support response time",
-          "Marketing automations",
-        ],
-        mostPopular: true,
-      },
-    ],
-  },
-];
+const packageData = package_detail_data;
 </script>
 
 <style scoped></style>
