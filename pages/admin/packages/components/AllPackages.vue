bb45d4ffb68dbfcce117c818814771e842f45872
katalyst33 <>
2023-08-21 15:16:43 +0100

    Create AllPackages.vue

---
+++ pages/admin/packages/components/AllPackages.vue
@@ -0,0 +1,22 @@
+<template>
+   <!-- you will need to handle a loading state -->
+   <div v-if="pending">Loading ...</div>
+    <div v-else>
+      <div v-for="item in data" :key="item.uuid">
+        <!-- do something -->
+
+        {{ item }}
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useAxiosFetch } from "../../../../http";
+
+const [pending, getData, data, error] = useAxiosFetch("packages/all");
+onMounted(getData);
+
+
+</script>
+
+<style scoped></style>
