c533f067bbed1e2c92dab7613524a2543cf94f04
katalyst33 <>
2023-08-21 16:16:52 +0100

    added time ago component

--- components/TimeAgo.vue
+++ components/TimeAgo.vue
@@ -1,14 +1,21 @@
 <template>
-  <div >{{ value }}</div>
+  <div> {{ formattedTime }}</div>
 </template>
 
 <script setup lang="ts">
+
+import moment from "moment";
 const props = defineProps({
   value: {
     type: String,
     required: true,
   },
 });
+
+const formattedTime = computed(() => {
+  return moment(props.value).fromNow();
+});
+
 </script>
 
 <style scoped></style>
