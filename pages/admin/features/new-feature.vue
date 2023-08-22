06eac0e48451b6189dbaf380967bebecbf672a4c
katalyst33 <>
2023-08-23 00:28:43 +0100

    can add new feature

--- pages/admin/features/new-feature.vue
+++ pages/admin/features/new-feature.vue
@@ -15,7 +15,17 @@
             <input type="text" v-model="form.name" />
           </div>
 
-          <div class="relative">
+          <div class="flex gap-x-4">
+            <button v-if="!isEditing" @click="editList">Edit</button>
+            <button v-else @click="previewList">preview</button>
+          </div>
+          <template v-if="!isEditing">
+            <ol class="list-item" type="1">
+              <li v-for="item in previewFeatures">> {{ item }}</li>
+            </ol>
+          </template>
+
+          <div v-else class="relative">
             <label>Features:</label>
             <textarea
               type="text"
@@ -27,7 +37,9 @@
           </div>
         </div>
 
-        <button @click="createFeature" class="btn p-4">save feature</button>
+        <button v-if="isEditing" @click="createFeature" class="btn p-4">
+          save feature
+        </button>
       </div>
     </div>
   </div>
@@ -35,8 +47,7 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-
-import { $useFetchApi } from "../../../http";
+import { $useAdminFetchApi } from "../../../http";
 
 definePageMeta({
   name: "new-feature",
@@ -44,25 +55,64 @@ definePageMeta({
 });
 
 const existingArray = ref([]);
-
-const form = ref({
-  name: "",
-  list: "",
+const form = reactive<{
+  slug: string;
+  name: string;
+  list: string;
+}>({
+  slug: "INTERIOR",
+  name: "INTERIOR DETAILING",
+  list: `Vacuum 
+  trunk 
+  stain removal
+  leather/fabric cleaning & conditioning
+  mat/rugs shampooing
+  vinyl/plastic restoration
+  door jambs
+  dashboard
+  roof
+  AC vents flushing & steaming,cockpits`,
 });
 
+const isEditing = ref(true);
+
+const previewFeatures = ref([] as string[]);
+
+function previewList() {
+  isEditing.value = false;
+  console.log(form.list.split("\n"));
+  previewFeatures.value = form.list.split("\n");
+}
+
+function editList() {
+  isEditing.value = true;
+  console.log(form.list.split("\n"));
+
+  form.list = previewFeatures.value.join("\n");
+}
+
 function createFeature() {
-  $useFetchApi({
-    url: "packages/add-feature",
+  $useAdminFetchApi({
+    url: "features/new",
     method: "POST",
-    data: form.value,
+    data: form,
   })
-    .then((res) => {
+    .then((res: any) => {
       console.log(res);
     })
-    .catch((err) => {
+    .catch((err: any) => {
       console.log(err);
     });
 }
+
+/* const form = reactive({
+  name: "",
+  price: 0.0,
+  enabled: true,
+  list: [] as string[]
+}); */
+
+const editFeaturesText = ref<string>();
 </script>
 
 <style scoped>
