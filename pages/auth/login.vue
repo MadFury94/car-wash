daea34dbc20242f3161795285bddeeef9c6c7ee6
katalyst33 <>
2023-08-30 02:14:58 +0100

    save

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -44,15 +44,10 @@
                 class="block text-sm font-medium leading-6 text-gray-900"
                 >Password</label
               >
-
               <input
                 v-model="form.password"
-                id="password"
-                name="password"
                 type="password"
                 autocomplete="current-password"
-                required=""
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -70,7 +65,6 @@
 
 <script setup lang="ts">
 import { reactive } from "vue";
-import { $useBaseApi } from "~/http";
 import { useAuthStore } from "~/store/auth.store";
 
 definePageMeta({
@@ -93,7 +87,6 @@ async function login() {
   }>(form);
 
   await authStore.setCurrentUserToken(res.token);
-
 
   alert("Login success");
 }
