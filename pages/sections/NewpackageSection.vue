38792d94bdac05567c915552a0dccaaa162a7f42
MadFury94 <onochieazukaeme@gmail.com>
2023-08-30 07:05:33 +0100

    change

--- pages/sections/NewpackageSection.vue
+++ pages/sections/NewpackageSection.vue
@@ -1,55 +1,49 @@
 <template>
-  <div class="mx-auto max-w-4xl text-center">
-    <PillInfo title="PACKAGES" class="flex justify-center" />
-    <p class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-      Choose your Package
-    </p>
-  </div>
-
-  <div v-if="pending">Loading ...</div>
-  <template v-else>
-    <div v-if="data" class="mt-16 flex justify-center">
-      <RadioGroup
-        v-model="selectedTier"
-        :class="menuStyle"
-        class="rounded-full p-1 text-center text-xs font-semibold leading-5 ring-1 ring-inset ring-gray-200"
-      >
-        <RadioGroupLabel class="sr-only">Payment frequency</RadioGroupLabel>
-        <RadioGroupOption
-          as="template"
-          v-for="option in Object.keys(data)"
-          :key="option"
-          :value="option"
-          v-slot="{ checked }"
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl lg:px-8">
+      <div class="mx-auto max-w-4xl text-center">
+        <PillInfo title="PACKAGES" class="flex justify-center" />
+        <p
+          class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"
         >
-          <div
-            :class="[
-              checked ? 'bg-secondary-500 text-white' : 'text-gray-500',
-              'cursor-pointer rounded-full px-2.5 py-1 ',
-            ]"
+          Choose your Package
+        </p>
+        <div class="mt-4 flex justify-center">
+          <button
+            @click="selectedTier = 'saloon'"
+            :class="
+              selectedTier === 'saloon'
+                ? 'bg-secondary-500 text-white'
+                : 'text-gray-500'
+            "
+            class="px-4 py-2 rounded-l-md"
           >
-            <span>{{ option }}</span>
-          </div>
-        </RadioGroupOption>
-      </RadioGroup>
-    </div>
-    <div v-if="data">
-      <div
-        v-for="(tier, index) in data[selectedTier]"
-        :key="index"
-        :class="[
-          tier.mostPopular
-            ? 'ring-2 ring-secondary-600'
-            : 'ring-1 ring-gray-200',
-          'rounded-3xl mx-auto max-w-lg',
-        ]"
-        class="flex-none mx-4"
-      >
-        <div>
-          <div
-            class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none"
+            Saloon
+          </button>
+          <button
+            @click="selectedTier = 'suv'"
+            :class="
+              selectedTier === 'suv'
+                ? 'bg-secondary-500 text-white'
+                : 'text-gray-500'
+            "
+            class="px-4 py-2 rounded-r-md"
           >
-            <div class="p-8 sm:p-10 lg:flex-auto">
+            SUV
+          </button>
+        </div>
+      </div>
+
+      <div v-if="pending" class="text-center mt-8">Loading ...</div>
+
+      <template v-else>
+        <div v-if="data" class="mt-8">
+          <div class="flex flex-col gap-8">
+            <div
+              v-for="(tier, index) in data[selectedTier]"
+              :key="index"
+              class="border rounded-lg p-8 hover:shadow-lg transition-shadow"
+            >
               <h3
                 :class="[
                   tier.mostPopular ? 'text-secondary-600' : 'text-gray-900',
@@ -58,64 +52,42 @@
               >
                 {{ tier.name }}
               </h3>
-              <p>
+              <p class="mt-2 text-sm text-gray-600">
                 <i class="fa-regular fa-timer text-secondary-500 pr-2"></i
                 >{{ tier.duration }}
               </p>
-              <div class="mt-10 flex items-center gap-x-4">
-                <h4
-                  class="flex-none text-sm font-semibold leading-6 text-indigo-600"
-                >
-                  What’s included
-                </h4>
-                <div class="h-px flex-auto bg-gray-100" />
-              </div>
-              <ul
-                role="list"
-                class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
-              >
+
+              <p class="mt-4 text-xl font-bold">₦{{ tier.price }}</p>
+              <ul class="mt-4 space-y-3 text-sm leading-6 text-gray-600">
                 <li v-for="(feature, index) in tier.features" :key="index">
-                  <CheckIcon
-                    class="h-6 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  />
-                  {{ feature.name }}
+                  <div class="flex items-center gap-x-2">
+                    <div
+                      class="bg-secondary-500 p-2 rounded-full h-6 w-6 flex justify-center items-center"
+                    >
+                      <i class="fa-solid fa-caret-down text-white"></i>
+                    </div>
+                    <span>{{ feature.name }}</span>
+                  </div>
                 </li>
               </ul>
-            </div>
-            <div
-              class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0"
-            >
-              <div
-                class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16"
-              >
-                <div class="mx-auto max-w-xs px-8">
-                  <p class="text-base font-semibold text-gray-600">
-                    Pay once, own it forever
-                  </p>
-                  <p class="mt-6 flex items-baseline gap-x-1">
-                    <span
-                      class="text-4xl font-bold tracking-tight text-gray-900"
-                      >₦{{ tier.price }}</span
-                    >
-                  </p>
-                  <a
-                    href="#"
-                    class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >Get access</a
-                  >
-                  <p class="mt-6 text-xs leading-5 text-gray-600">
-                    Invoices and receipts available for easy company
-                    reimbursement
-                  </p>
-                </div>
+
+              <div class="mt-6 flex flex-col items-center gap-4">
+                <button
+                  @click="createBooking(tier.uuid)"
+                  class="w-full px-4 py-2 bg-secondary-600 text-white rounded-lg hover:bg-secondary-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-400"
+                >
+                  Book Now
+                </button>
+                <p v-if="tier.mostPopular" class="text-xs text-red-600">
+                  Most Popular!
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </template>
     </div>
-  </template>
+  </div>
 </template>
 
 <script setup lang="ts">
