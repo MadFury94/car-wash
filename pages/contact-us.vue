d0ca6f75645dffbc55cd41f45c0ff5b582f1ac5d
MadFury94 <onochieazukaeme@gmail.com>
2023-09-01 17:25:44 +0100

    contact 80%

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="lg:flex gap-x-7">
-      <div class="bg-white p-24">
+      <div class="bg-white p-24 grid grid-">
         <PillInfo class="uppercase py-6" title="Contact Info" />
         <h1 class="heading">Contact Details</h1>
         <p>
@@ -59,6 +59,37 @@
             />
           </div>
         </div>
+        <div class="mt-24">
+          <h2
+            class="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight"
+          >
+            Book an Appointment
+          </h2>
+          <div class="mt-3">
+            <p class="text-lg text-gray-500">
+              Contact Our Head Office for Bookings
+            </p>
+          </div>
+          <div class="mt-9">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <i class="fa-solid fa-phone text-gray-400"></i>
+              </div>
+              <div class="ml-3 text-base text-gray-500">
+                <p>+1 (555) 123 4567</p>
+                <p class="mt-1">Available 24/7</p>
+              </div>
+            </div>
+            <div class="mt-6 flex">
+              <div class="flex-shrink-0">
+                <i class="fa-solid fa-envelope text-gray-400"></i>
+              </div>
+              <div class="ml-3 text-base text-gray-500">
+                <p>fraudreport@youronlinebanker.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="p-20">
@@ -101,86 +132,58 @@
                 class="block mb-2 text-sm font-medium text-gray-900"
                 >Your message</label
               >
-              <input
-                class="border-secondary-900 border-2 rounded-md w-full form-textarea"
-                placeholder="Enter your message"
-                type="textarea"
-                :rows="8"
-              />
+
+              <div class="mt-2.5">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="4"
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
           </div>
         </div>
         <ButtonComponent class="mt-10" title="Submit" />
+
+        <div class="mt-24">
+          <h2
+            class="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight"
+          >
+            General Support
+          </h2>
+          <div class="mt-3">
+            <p class="text-lg text-gray-500">
+              For general queries and assistance regarding our Car Wash
+            </p>
+          </div>
+          <div class="mt-9">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <i class="fa-solid fa-phone text-gray-400"></i>
+              </div>
+              <div class="ml-3 text-base text-gray-500">
+                <p>+1 (555) 123 4567</p>
+                <p class="mt-1">Mon-Fri 8am to 6pm PST</p>
+              </div>
+            </div>
+            <div class="mt-6 flex">
+              <div class="flex-shrink-0">
+                <i class="fa-solid fa-envelope text-gray-400"></i>
+              </div>
+              <div class="ml-3 text-base text-gray-500">
+                <p>support@youronlinebanker.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="bg-white">
       <div class="mx-auto max-w-7xl px-6 py-16 lg:px-8">
         <div
           class="mx-auto max-w-lg md:grid md:max-w-none md:grid-cols-2 md:gap-8"
-        >
-          <div>
-            <h2
-              class="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight"
-            >
-              Book an Appointment
-            </h2>
-            <div class="mt-3">
-              <p class="text-lg text-gray-500">
-                Contact Our Head Office for Bookings
-              </p>
-            </div>
-            <div class="mt-9">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <i class="fa-solid fa-phone text-gray-400"></i>
-                </div>
-                <div class="ml-3 text-base text-gray-500">
-                  <p>+1 (555) 123 4567</p>
-                  <p class="mt-1">Available 24/7</p>
-                </div>
-              </div>
-              <div class="mt-6 flex">
-                <div class="flex-shrink-0">
-                  <i class="fa-solid fa-envelope text-gray-400"></i>
-                </div>
-                <div class="ml-3 text-base text-gray-500">
-                  <p>fraudreport@youronlinebanker.com</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="mt-12 sm:mt-16 md:mt-0">
-            <h2
-              class="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight"
-            >
-              General Support
-            </h2>
-            <div class="mt-3">
-              <p class="text-lg text-gray-500">
-                For general queries and assistance regarding our Car Wash
-              </p>
-            </div>
-            <div class="mt-9">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <i class="fa-solid fa-phone text-gray-400"></i>
-                </div>
-                <div class="ml-3 text-base text-gray-500">
-                  <p>+1 (555) 123 4567</p>
-                  <p class="mt-1">Mon-Fri 8am to 6pm PST</p>
-                </div>
-              </div>
-              <div class="mt-6 flex">
-                <div class="flex-shrink-0">
-                  <i class="fa-solid fa-envelope text-gray-400"></i>
-                </div>
-                <div class="ml-3 text-base text-gray-500">
-                  <p>support@youronlinebanker.com</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        ></div>
       </div>
     </div>
   </div>
